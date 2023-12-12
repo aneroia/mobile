de7bc91e7d3e3014b4616cefbd86afece7e1bb88
Anna <334230@niuitmo.ru>
2023-12-12 17:19:09 +0300

    2

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.myapp
-
+import android.widget.EditText
+import android.widget.Button
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userNameEditText: EditText
+    private lateinit var walletAmountEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userNameEditText = findViewById(R.id.usernameEditText)
+        walletAmountEditText = findViewById(R.id.balanceEditText)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            saveUserDataAndNavigateToSecondActivity()
+        }
+    }
+
+    private fun saveUserDataAndNavigateToSecondActivity() {
+        val userName = userNameEditText.text.toString()
+        val walletAmount = walletAmountEditText.text.toString().toFloatOrNull()
+
+        if (userName.isNotEmpty() && walletAmount != null) {
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra("userName", userName)
+                putExtra("walletAmount", walletAmount)
+            }
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Проверьте правильность заполнения всех полей", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
---
+++ app/src/main/java/com/example/myapp/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapp
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+// В SecondActivity.kt
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val username = intent.getStringExtra("userName")
+        val balance = intent.getFloatExtra("walletAmount", 0f)
+
+        val userDataTextView = findViewById<TextView>(R.id.userDataTextView)
+        userDataTextView.text = "Имя пользователя: $username\nСумма кошелька: $balance"
+    }
+}
