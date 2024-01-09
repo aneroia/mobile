606c703c8bfd991bd54db3b5e3d090f0ab7113c7
Anna <334230@niuitmo.ru>
2024-01-10 01:17:54 +0300

    2

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -8,14 +8,12 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var userNameEditText: EditText
-    private lateinit var walletAmountEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         userNameEditText = findViewById(R.id.usernameEditText)
-        walletAmountEditText = findViewById(R.id.balanceEditText)
 
         val startButton = findViewById<Button>(R.id.startButton)
         startButton.setOnClickListener {
@@ -25,12 +23,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveUserDataAndNavigateToSecondActivity() {
         val userName = userNameEditText.text.toString()
-        val walletAmount = walletAmountEditText.text.toString().toFloatOrNull()
 
-        if (userName.isNotEmpty() && walletAmount != null) {
+
+        if (userName.isNotEmpty()) {
             val intent = Intent(this, SecondActivity::class.java).apply {
                 putExtra("userName", userName)
-                putExtra("walletAmount", walletAmount)
             }
             startActivity(intent)
         } else {
--- app/src/main/java/com/example/myapp/SecondActivity.kt
+++ app/src/main/java/com/example/myapp/SecondActivity.kt
@@ -1,24 +1,41 @@
 package com.example.myapp
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-// В SecondActivity.kt
-class SecondActivity : AppCompatActivity() {
 
+class SecondActivity : AppCompatActivity() {
+    private lateinit var share1summ: EditText
+    private lateinit var share2summ: EditText
+    private lateinit var share3summ: EditText
+    private lateinit var balanceTextView: TextView
+    private lateinit var updateButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         val username = intent.getStringExtra("userName")
-        val balance = intent.getFloatExtra("walletAmount", 0f)
-
+        share1summ = findViewById(R.id.share1summ)
+        share2summ = findViewById(R.id.share2summ)
+        share3summ = findViewById(R.id.share3summ)
+        balanceTextView = findViewById(R.id.balanceTextView)
+        updateButton = findViewById(R.id.updateButton)
         val userDataTextView = findViewById<TextView>(R.id.userDataTextView)
-        val balanceTextView = findViewById<TextView>(R.id.balanceTextView)
         userDataTextView.text = "Имя пользователя: $username"
-        balanceTextView.text = "Сумма кошелька: $balance"
+        val sum = 0
+        balanceTextView.text = "Сумма кошелька: $sum"
+        updateButton.setOnClickListener {
+            val share1Value = share1summ.text.toString().toDoubleOrNull() ?: 0.0
+            val share2Value = share2summ.text.toString().toDoubleOrNull() ?: 0.0
+            val share3Value = share3summ.text.toString().toDoubleOrNull() ?: 0.0
+
+            val sum = share1Value + share2Value + share3Value
+
+            balanceTextView.text = "Сумма кошелька: $sum"
+
+        }
 
     }
-
-    
 }
